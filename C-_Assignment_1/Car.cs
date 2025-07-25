d9a717fa5972db8b8eeb0382982c27d6fca636d9
Asif Ahmad <asif.zoma.ahmad@gmail.com>
2025-07-25 11:14:09 +0100

    Create Car.cs
    created the necessary files: Car.cs to implement the Car class

---
+++ C-_Assignment_1/Car.cs
@@ -0,0 +1,17 @@
+public class Car
+{
+    private string make;
+    private string model;
+    private string registration;
+    private int year;
+    private double currentValue;
+
+    public Car(string make, string model, string registration, int year, double currentValue)
+    {
+        this.make = make;
+        this.model = model;
+        this.registration = registration;
+        this.year = year;
+        this.currentValue = currentValue;
+    }
+} 
